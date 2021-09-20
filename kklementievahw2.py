783b5cec73e10b3a8c2da47ddc924c393d8c83f4
kklementieva <91091906+kklementieva@users.noreply.github.com>
2021-09-20 22:42:55 +0300

    Add files via upload

---
+++ kklementievahw2.py
@@ -0,0 +1,24 @@
+#  BEGIN
+print(8 / 2 + 5 - -3 / 2)
+#  END
+
+#  BEGIN
+print('Khal Drogo\'s favorite word is "athjahakar"')
+#  END
+
+#  BEGIN
+print("Winter"+" "+"came"+" "+"for"+" "+"the"+" "+"House"+" "+"of"+" "+"Frey"+".")
+#  END
+
+#  BEGIN
+word = "First text"
+print(word)
+word = "Second text"
+print(word)
+#  END
+
+#  BEGIN
+print("What is your name?")
+name = input()
+print("Hello,"+name+"!")
+#  END
